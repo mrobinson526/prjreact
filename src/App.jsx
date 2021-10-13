d878662fd4da4c186a4e48376d5fcf3252011bfc
Margaret <mrobinson526@gmail.com>
2021-10-13 12:16:00 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Greeter from './components/Greeter';
+import React, { useState } from 'react'; 
 const App = () => {
+    const [username, setUsername] = useState(' '); 
+    const [loaded, setLoaded] = useState(false);
+    function loading() {
+        if (loaded === false) {
+            <h1>Website loading ...</h1>
+        }
+            <h1>Loading complete</h1>
+    }
+    
     return (
         <div>
             <h1>This is Objective 1 of Simple Tasks a la React</h1>
-            <Greeter phrase='Hello there ' name ='Cohort15'/>
-            <Greeter phrase='Hello there ' name ='Stack Web Dev'/>
-            <Greeter phrase='Hello there ' name ='Data Analytics'/>
+            <Greeter phrase='Hello there' name ='Cohort15'/>
+            <Greeter phrase='Hello there' name ='Stack Web Dev'/>
+            <Greeter phrase='Hello there' name ='Data Analytics'/>
+            <input value={username} onChange={e => setUsername(e.target.value)} />
+            <p>You are Logging in as: {username}</p>
+            
         </div>
     );
 };
+
 export default App;
